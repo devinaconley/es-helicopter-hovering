ac8987017b5378f2c87abd120e23a2f0a88cdaf8
Devin Conley <devinconley@jhu.edu>
2017-04-19 01:54:42 -0400

    Setup conda environment, skeleton for open ai task

--- run.py
+++ run.py
@@ -3,16 +3,25 @@
 
 # lib
 import argparse
+import gym
 
 # src
 from src.ESTrainer import ESTrainer
 
 # main
-def main() :
+def main( ) :
 	# pull command line args
-	args = ParseArguments()
+	args = ParseArguments( )
 
+	# setup environment
+	env = gym.make( args['environment'] )
+	obs = env.reset( )
+	done = False
 
+	while not done :
+		env.render()
+		action = env.action_space.sample()
+		obs, reward, done, info = env.step( action )
 
 # Command Line Arguments
 def ParseArguments( ) :
@@ -20,10 +29,12 @@ def ParseArguments( ) :
 	parser = argparse.ArgumentParser( )
 	parser.add_argument( '-m', '--model', help='Path to existing model to start training from',
 						 default=None )
-	parser.add_argument( '-e', '--episodes', help='Number of episodes per training iterations',
+	parser.add_argument( '-n', '--episodes', help='Number of episodes per training iteration',
 						 default=1000 )
 	parser.add_argument( '-i', '--iterations', help='Max number of training iterations',
 						 default=1000 )
+	parser.add_argument( '-e', '--environment', help='OpenAI environment',
+						 default='LunarLander-v2' )
 
 	# Parse arguments and return
 	args = vars( parser.parse_args( ) )
