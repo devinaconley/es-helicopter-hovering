ea7dbfbe4c258179ba5a0b7a3cac09bc13989352
e <55wuyong@163.com>
2017-05-12 20:33:16 -0400

    fix bug

--- src/MetaLearner.py
+++ src/MetaLearner.py
@@ -27,26 +27,24 @@ class MetaLearner :
                 trainer = ESTrainer( m, self.env )
 
                 # test, append to metaParams
-                reward = trainer.Train(iterations=10, population=pop, sigma=sigma, lr=lr, render=True)
+                reward = trainer.Train(iterations=5, population=pop, sigma=sigma, lr=lr, render=True)
                 w = trainer.model.get_weights()
                 metaParams.append([reward, lr, sigma, w])
 
                 print("metaparams is {}".format(metaParams[-1]))
-                lr += np.random.normal(0, sigmaMeta_lr)  # + noise
-                sigma += np.random.normal(0, sigmaMeta_sigma)  # + noise
+                if(len(metaParams)!=popMeta):
+                    lr += np.random.normal(0, sigmaMeta_lr)  # + noise
+                    sigma += np.random.normal(0, sigmaMeta_sigma)  # + noise
 
             # set self.model as best model, update lr and sigma with reward weighting
-            max = metaParams[0][0]
+            maxReward = metaParams[0][0]
             wts = metaParams[0][-1]
             for meta in metaParams:
-                if max < meta[0]:
-                    max = meta[0]
+                if maxReward < meta[0]:
+                    maxReward = meta[0]
                     lr = meta[1] # + np.random.normal(0, sigmaMeta_lr) * lrMeta
                     sigma = meta[2] # + np.random.normal(0, sigmaMeta_sigma) * lrMeta
                     wts = meta[-1]
             self.model.set_weights(wts)
 
-            print ("max reward is {}, lr is {}, sigma is {}".format(max, lr, sigma))
-
-
-
+            print ("max reward is {}, lr is {}, sigma is {}".format(maxReward, lr, sigma))
