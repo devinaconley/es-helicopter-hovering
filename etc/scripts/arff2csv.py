7cc37f40d08027941a2b57a7dd4b43bf993d426d
Devin A. Conley <devinaconley@gmail.com>
2018-01-11 01:42:07 -0600

    utility script for converting arff to csv

---
+++ etc/scripts/arff2csv.py
@@ -0,0 +1,39 @@
+# utility script to convert arff file to csv
+
+# lib
+import argparse
+import csv
+import arff
+import os
+
+def main() :
+	args = parseArguments()
+
+	with open( args['arff'] ) as file :
+		dataset = arff.load( file )
+
+	csvPath = args['csv'] if args['csv'] else os.path.splitext( args['arff'] )[0] + '.csv'
+	with open( csvPath, 'w' ) as file :
+		writer = csv.writer( file )
+		writer.writerow( [a[0] for a in dataset['attributes']] )
+		writer.writerows( dataset['data'] )
+
+# command line arguments
+def parseArguments() :
+	# define arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument( '-a', '--arff', help='Path to arff data file',
+						 default=None )
+	parser.add_argument( '-c', '--csv', help='Path to converted csv output file',
+						 default=None )
+
+	# parse arguments and validate
+	args = vars( parser.parse_args() )
+
+	if args['arff'] is None :
+		parser.error( 'Data file must be provided.' )
+
+	return args
+
+if __name__ == '__main__' :
+	main()
